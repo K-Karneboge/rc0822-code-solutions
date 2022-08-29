74862fcc851d47ea69916f16cd788b698ce67f1f
Kevin Karneboge <kkarneboge@gmail.com>
2022-08-29 16:59:49 +0000

    JS primitives and variables exersise completed

--- javascript-primitives-and-variables/main.js
+++ javascript-primitives-and-variables/main.js
@@ -13,3 +13,15 @@ if (isCool === true) {
 }
 
 console.log(fullName + ' has ' + totalPets + ' pets.');
+
+console.log('fullName is a/n ' + typeof (fullName));
+console.log('isCool is a/n ' + typeof (isCool));
+console.log('totalPets is a/n ' + typeof (totalPets));
+
+var unknown = null;
+console.log(unknown);
+console.log('unknown is a/n ' + typeof (unknown));
+
+var never;
+console.log(never);
+console.log('never is a/n ' + typeof (never));
