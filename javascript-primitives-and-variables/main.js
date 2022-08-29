d16c9d39cacf880abd35a07ebfebe1a8bb95c671
Kevin Karneboge <kkarneboge@gmail.com>
2022-08-29 16:50:38 +0000

    JS primitives and variables exersise completed

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,15 @@
+var fullName = 'Kevin Karneboge';
+var isCool = false;
+var totalPets = 2;
+
+console.log('My name is ' + fullName);
+
+if (isCool === true) {
+  console.log(fullName + ' is not cool');
+} else if (isCool === false) {
+  console.log(fullName + ' is not cool');
+} else {
+  console.log('It is unknown whether ' + fullName + ' is cool or not.');
+}
+
+console.log(fullName + ' has ' + totalPets + ' pets.');
