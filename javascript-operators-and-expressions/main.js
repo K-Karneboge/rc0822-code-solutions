45344cf2c3009234be0e727202c92f9e99f99d6a
Kevin Karneboge <kkarneboge@gmail.com>
2022-08-29 18:35:04 +0000

    done exersises operators and expressions

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,39 @@
+var width = 5;
+var height = 4;
+var area = width * height;
+console.log(area);
+console.log(typeof area);
+
+var bill = 10;
+var payment = 3.14;
+
+var change = bill - payment;
+console.log(change);
+console.log(typeof change);
+
+var quizzes = 80;
+var midterm = 87;
+var final = 98;
+var grade = (quizzes + midterm + final) / 3;
+console.log(grade);
+console.log(typeof grade);
+
+var firstName = 'Kevin';
+var lastName = 'Karneboge';
+var fullName = firstName + ' ' + lastName;
+console.log(fullName);
+console.log(typeof fullName);
+
+var ph = 0;
+var isAcidic = ph < 7;
+console.log(isAcidic);
+console.log(typeof isAcidic);
+
+var headCount = 247;
+var isSparta = headCount === 300;
+console.log(isSparta);
+console.log(typeof isSparta);
+
+var motto = fullName + ' is the GOAT';
+console.log(motto);
+console.log(typeof motto);
