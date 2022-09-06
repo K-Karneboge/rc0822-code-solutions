2a490d6946dc47392a024b017c9e9c41860b527e
Kevin Karneboge <kkarneboge@gmail.com>
2022-09-06 13:23:08 +0000

    I dont know how much of it was supposed to be in main.js with instructions mostly in console

---
+++ dom-querying/main.js
@@ -0,0 +1,11 @@
+console.log('helloworld');
+
+console.log(document.querySelector('h1'));
+
+document.querySelector('#explanation');
+
+console.log(document.querySelector('#explanation'));
+console.log(document.querySelector('.hint'));
+
+console.log(document.querySelectorAll('p'));
+console.log(document.querySelectorAll('.example-link'));
