eb820b98d86b55e1c50fe771b15faca6b9b251f2
Kevin Karneboge <kkarneboge@gmail.com>
2022-09-08 18:10:01 +0000

    re added the console logs

--- dom-event-delegation/main.js
+++ dom-event-delegation/main.js
@@ -1,8 +1,10 @@
 
 var taskList = document.querySelector('.task-list');
-
+console.log(document.querySelector('.task-list'));
 function handleClick(event) {
   if (event.target.matches('button')) {
+    console.log('button found, removing the following: ');
+    console.log('closest task list item is:', event.target.closest('.task-list-item'));
     event.target.closest('.task-list-item').remove();
   }
 }
