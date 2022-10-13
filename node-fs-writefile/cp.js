b098e846fda7a373405e976cec6705d34fa34ab5
Kevin Karneboge <kkarneboge@gmail.com>
2022-10-13 22:01:32 +0000

    completed copying module

---
+++ node-fs-writefile/cp.js
@@ -0,0 +1,18 @@
+var fs = require('fs');
+const copied = String(process.argv[2]);
+const pasted = String(process.argv[3]);
+
+let copiedThing = null;
+fs.readFile(copied, (err, data) => {
+  if (err) {
+    throw err;
+  }
+  copiedThing = String(data);
+  fs.writeFile(pasted, copiedThing, err => {
+    if (err) { throw err; }
+  });
+});
+
+// fs.writeFile('note.txt', note, 'utf8', err => {
+//   if (err) throw err;
+// });
