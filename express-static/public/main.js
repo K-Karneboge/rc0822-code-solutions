5cb52ee6ac7354814f8ad8809d68b377a4e0c08f
Kevin Karneboge <kkarneboge@gmail.com>
2022-11-01 17:02:25 +0000

    fixed my test js function

--- express-static/public/main.js
+++ express-static/public/main.js
@@ -2,4 +2,4 @@ const test = document.querySelector('.test');
 function toggle(e) {
   test.className = 'test2';
 }
-document.addEventListener('click', toggle);
+test.addEventListener('click', toggle);
